2377a89a5553f2e11f22544e0e939201191e2434
merlinbecker <merlin.becker@googlemail.com>
2018-11-21 14:41:29 +0100

    OAUTH 2

--- index.php
+++ index.php
@@ -20,39 +20,51 @@ ob_start();
  * @TODO make a helper class out of this (and the absolute url)
  * **/
 
-$accepts=explode(", ",$_SERVER['HTTP_ACCEPT']);
-$method=$_SERVER['REQUEST_METHOD'];
-$url_prefix=str_replace("index.php","",$_SERVER['SCRIPT_NAME']);
-$temp_url="/".str_replace($url_prefix, "", $_SERVER['REQUEST_URI']);
+/**
+* parseServerArguments: übernimmt das Extrahieren der Kommandoparameter aus den Servervariablen
+* @return Command and Query Params als Array
+**/
+function parseServerArguments(){
+	$output=array();
+	$output['accept']=explode(", ",$_SERVER['HTTP_ACCEPT']);
+	$output['method']=$_SERVER['REQUEST_METHOD'];
+	
+	$url_prefix=str_replace("index.php","",$_SERVER['SCRIPT_NAME']);
+	$temp_url="/".str_replace($url_prefix, "", $_SERVER['REQUEST_URI']);
 
 
-//remove query from request uri
-$query_params=substr($temp_url,strpos($temp_url,"?")+1);
-if(!strpos($temp_url,"?")===false)
-	$req_uri=substr($temp_url,0,strpos($temp_url,"?"));
-else 
-	$req_uri=$temp_url;
-$tempparams=explode("/",$req_uri);
-$params=array();
-$params[]=$tempparams[1];
-$params[]=$tempparams[2];
-$params[]=$tempparams[3];
-$url_query=array();
+	//remove query from request uri
+	$query_params=substr($temp_url,strpos($temp_url,"?")+1);
+	if(!strpos($temp_url,"?")===false)
+		$req_uri=substr($temp_url,0,strpos($temp_url,"?"));
+	else 
+		$req_uri=$temp_url;
+	$tempparams=explode("/",$req_uri);
+	$params=array();
+	for($i=0;$i<count($tempparams);$i++){
+		if($tempparams[$i]!="")
+			$params[]=$tempparams[$i];
+	}
+	
+	$output['commands']=$params;
 
-parse_str($query_params,$url_query);
+	$url_query=array();
+	parse_str($query_params,$url_query);
 
+	if($_SERVER['argc']>0){	
+		parse_str($_SERVER['argv'][0],$url_query);
+	}
+	$output['query']=$url_query;
 
-if($_SERVER['argc']>0){	
-	echo "hier her!!";
-	parse_str($_SERVER['argv'][0],$url_query);
+	return $output;
 }
+
 
 
 echo "index!";
 
 echo "<pre>";
-print_r($params);
-print_r($url_query);
+print_r(parseServerArguments());
 echo "</pre>";
 
 ?>
